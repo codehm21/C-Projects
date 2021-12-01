4264ae38c2faab99949d16a81c87eff3c907b92c
Haroon <codehm21@gmail.com>
2021-12-01 13:57:57 +0000

    Notified cashier how many £20 £10 & £5 notes to give back in change

---
+++ cashier.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main (void)
+
+{
+    /*
+    int Fiftynotes = get_int("how many 50 notes in the till?: ");
+    */    
+    float cog = get_float("Amount owed: £");
+    float paid = get_float("Amount paid: £");
+    float change = (paid-cog);
+    const int five = 5, ten = 10, twenty = 20, fifty = 50;
+    int fi = 0, te = 0, twen = 0, fif = 0;
+
+    {
+        printf("Change: £%.2f", change);
+    printf("\n");
+    }
+
+
+    //£20 Notes
+
+    while(twenty <= change)
+    for (twenty; twenty<=change; change = change - twenty)
+    {
+        twen++;
+    }
+    {
+        printf("Give %i Twenty pound notes", twen);
+    }
+printf("\n");
+
+
+    //£10 Notes
+
+    while(ten <= change)
+    for (ten; ten<=change; change = change - ten)
+    {
+        te++;
+    }
+    {
+        printf("Give %i Ten pound notes", te);
+    }
+printf("\n");
+
+    // £5 Notes
+        
+    while(five <= change)
+    for (five; five<=change; change = change - five)
+    {
+        fi++;
+    }
+    {
+        printf("Give %i Five pound notes", fi);
+    }
+printf("\n");
+
+
+
+}
+  
