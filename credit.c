bd1c6437801c0df9b8ddc5f39acdb665b6ae903c
Haroon <codehm21@gmail.com>
2021-12-07 18:31:28 +0000

    Checks lenghs and first digits to see if number entered could be American Express, Visa or Mastercard

--- credit.c
+++ credit.c
@@ -9,14 +9,19 @@ int main(void)
 
 {
    
-long long creditcard, ccnum;
-int lengh = 0;
+long long creditcard, ccnum, visacard;
+int lengh = 0, cclengh = 0, vlengh;
+
+    //Capture Card Number
 
 do
 
   { creditcard = get_long_long("Number: ");
-    ccnum = creditcard; 
+    ccnum = creditcard;
+    visacard = creditcard; 
   } while (ccnum < 0);
+
+  // Get Lengh of Card Number
 
 do
   {
@@ -24,27 +29,48 @@ do
     creditcard/=10;
   } while (creditcard != 0);
 
-  if (lengh !=3)
-  {
-    printf("INVALID \n");
-    return 1;
-  }
+  // Check Lengh of Card Number
 
-  for (lengh = 0; lengh < 1; lengh++)
-  {
-    ccnum /= 10;
-  }
-
-  if (ccnum != 34 && ccnum !=37) 
+  if (lengh == 15 || lengh == 16 || lengh == 13)
+{
+  // Get first 2 digits of America Express and Mastercard and Visa by dividing until 2 digits and 1 digit for visa
+ 
+    for (cclengh = 0; ccnum > 100; cclengh++)
     {
-      printf("INVALID \n");
-      return 1;
+        ccnum /= 10;
     }
-  else 
+    
+    for (vlengh = 0; visacard > 10; vlengh++)
     {
-    printf("American Express \n");
-    return 0;
-    }   
+        visacard /=10;
+    }
+
+  // Check to see if American Express, Visa or Mastercard
+
+    if ((ccnum == 34 || ccnum == 37 ) && (lengh == 15))
+    {
+        printf("American Express \n");
+        return 0;
+    }
+    else if ((ccnum > 50 && ccnum < 56) && (lengh == 16))
+    {
+        printf("Mastercard \n");
+        return 0;
+    }
+     else if ((visacard == 4) && (lengh = 13 || lengh == 16))
+    {
+        printf("VISA \n");
+        return 0;
+    }
+    else
+    {
+        printf("INVALID\n");
+        return 1;
+    }
+   
+}   printf("INVALID\n");
+
+
         
 }     
 
