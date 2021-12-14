16e06d9c284d29b1687969e349ff91219f42501c
Haroon <codehm21@gmail.com>
2021-12-14 12:52:15 +0000

    contains comments for future reference

--- subs.c
+++ subs.c
@@ -14,7 +14,7 @@ bool check_duplicates(string (s));
 
 int dup, p;
 int key [25], position [25];
-char Alphabet[25];
+
 
 // Main program
 
@@ -29,22 +29,37 @@ int main(int argc, string argv[])
         return 1;
     }
 
+// Get plaintext
+
     string sub = argv[1];
     string plaintext = get_string("plaintext: ");
+
+//Output Ciphertext    
     printf("Ciphertext: ");
+
+// Iterate over each plaintext, calculate how far each character is from 'A' or 'a' using that as a position of which character from Subsitution text should be printed
+
 
     for (int k = 0, lengh = strlen(plaintext); k < lengh; k++)
  
     {   
         char d = plaintext[k];
+
+// Check to see if alphabetic character else print as they are
+
         if(isalpha(d))
         {
+// If Character is uppercase its value is going to be higher so this needs to be accounted for by subtracting 'a' or A'            
             char m = 'A';
             if (islower (d))
             m = 'a';    
             p = d - m;
+// sub is the substitution key inputted, as from the above h is 7 away from a so [p] needs to = 7, sub[p] is the 7th position in the substitution key
+// therefore printing sub wherever input text position is away from 'a' or 'A'             
             printf("%c", sub[p]);
         }
+
+// ignore anything that is not alphabetic such as white space or , etc
     else
         printf("%c", d);
     }   
@@ -71,6 +86,7 @@ bool check_duplicates(string (s))
 {
     for (int i = 0, len = strlen(s); i < len; i++)
   
+//Convert string to one case; upper or lower otherwise lower case 'a' != 'A' and this will not initiate dup++
 
     {
         for (int j = i + 1, len = strlen(s); j < len; j++)   
